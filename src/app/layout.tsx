5c7d329e5fe651fc63d94a80805c17979ab647b5
Victor <victor.quertelet@gmail.com>
2025-08-08 10:43:15 +0200

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
@@ -33,30 +34,16 @@ export const metadata: Metadata = {
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='fr'>
-			<head>
-				{/* Google Tag Manager */}
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-					})(window,document,'script','dataLayer','GTM-PBCGC2PQ');`,
-					}}
-				/>
-				{/* End Google Tag Manager */}
-			</head>
 			<body className={`min-h-screen flex flex-col justify-between font-sans text-gray-900 ${inter.className}`}>
-				{/* Google Tag Manager (noscript) */}
-				<noscript>
-					<iframe 
-						src="https://www.googletagmanager.com/ns.html?id=GTM-PBCGC2PQ"
-						height="0" 
-						width="0" 
-						style={{display:'none',visibility:'hidden'}}
-					/>
-				</noscript>
-				{/* End Google Tag Manager (noscript) */}
+				<Script src="https://www.googletagmanager.com/gtag/js?id=G-G31JH48C4J" />
+				<Script id="google-analytics">
+					{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', 'G-G31JH48C4J');
+					`}
+				</Script>
 				{children}
 				<Analytics />
 				<SpeedInsights />
